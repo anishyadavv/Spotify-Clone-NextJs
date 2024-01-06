1d080d777ecf1ea4d6e2d11315a7e8da4f12ffe7
anish <anishyadav7853@gmail.com>
2024-01-06 20:40:56 +0530

    sidebar ui built

---
+++ app/(site)/page.tsx
@@ -0,0 +1,9 @@
+import Sidebar from "@/components/Sidebar"
+
+export default function Home() {
+  return (
+    <Sidebar>
+      <h1 className='text-green-500'>Spotify</h1>
+    </Sidebar>
+  )
+}
---
+++ components/SidebarItem.tsx
@@ -0,0 +1,42 @@
+import { IconType } from "react-icons"
+import Link from "next/link";
+import { twMerge } from "tailwind-merge";
+interface SidebarItemPrpos {
+    icon: IconType;
+    label: string;
+    active?: boolean;
+    href: string;
+}
+
+const SidebarItem: React.FC<SidebarItemPrpos> = ({
+    icon: Icon,
+    label,
+    active,
+    href
+}) => {
+  return (
+    <div>
+
+    <Link
+    href={href}
+    className={twMerge(`
+    flex
+    flex-row
+    h-auto
+    items-center
+    w-full
+    gap-x-4
+    text-md
+    font-medium
+    cursor-pointer
+    hover:text-white
+    transition
+    text-neutral-400
+    py-1`, active && 'text-white')}>
+      <Icon size={26}/> <p className="truncate w-full">{label}</p>
+    </Link>
+    </div>
+  )
+}
+
+export default SidebarItem
